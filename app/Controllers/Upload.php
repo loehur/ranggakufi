989d8f89b83e08538e4c2b35c28a3168788ac273
loehur <loehur@gmail.com>
2022-09-27 16:06:07 +0700

    error upload fix6

--- app/Controllers/Upload.php
+++ app/Controllers/Upload.php
@@ -54,7 +54,7 @@ class Upload extends Controller
                   $load_num = $line[17];
                   $singleMultiple_periods = $line[18];
                   $first_loanReloan = $line[19];
-                  $total_call = $line[20];
+                  $total_call = $line[20] || 0;
                   $tl = $line[21];
                   $om = $line[22];
 
@@ -63,7 +63,7 @@ class Upload extends Controller
                   $simID = str_replace("KUFI-", "", $employee_id);
                   $primary = $simDate . $simID . $allocated_amount . $total_repay_amount;
 
-                  $vals =  "'" . $primary . "','" . $date . "','" . $ticket_category . "','" . $ranking . "','" . $assign_to . "','" . $employee_id . "','" . $role . "','" . $allocated_amount . "','" . $repay_principal . "','" . $repay_interest . "','" . $total_repay_amount . "','" . $rate_of_return . "','" . $target_repay_rate . "','" . $diff_target_repay_amount . "','" . $new_assign_num . "','" . $handle_times . "','" . $handle_num . "','" . $complete_num . "','" . $load_num . "','" . $singleMultiple_periods . "','" . $first_loanReloan . "','" . $total_call . "','" . $tl . "','" . $om . "','" . $dateNow . "'";
+                  $vals =  "'" . $primary . "','" . $date . "','" . $ticket_category . "','" . $ranking . "','" . $assign_to . "','" . $employee_id . "','" . $role . "','" . $allocated_amount . "','" . $repay_principal . "','" . $repay_interest . "','" . $total_repay_amount . "','" . $rate_of_return . "','" . $target_repay_rate . "','" . $diff_target_repay_amount . "','" . $new_assign_num . "','" . $handle_times . "','" . $handle_num . "','" . $complete_num . "','" . $load_num . "','" . $singleMultiple_periods . "','" . $first_loanReloan . "'," . $total_call . ",'" . $tl . "','" . $om . "','" . $dateNow . "'";
                   $query = "INSERT INTO data_daily VALUES(" . $vals . ");";
                   $lastInsert = $realDate . " " . $employee_id;
 
