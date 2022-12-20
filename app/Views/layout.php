f472abd4e153d6a10c886d4a5467f59f07c0f391
loehur <loehur@gmail.com>
2022-12-20 16:20:39 +0700

    refresh interval

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -646,4 +646,17 @@ if (isset($data['pageInfo'])) {
         $("a.refresh").on('click', function() {
           location.reload(true);
         });
+
+        var time = new Date().getTime();
+        $(document.body).bind("mousemove keypress", function(e) {
+          time = new Date().getTime();
+        });
+
+        function refresh() {
+          if (new Date().getTime() - time >= 420000)
+            window.location.reload(true);
+          else
+            setTimeout(refresh, 10000);
+        }
+        setTimeout(refresh, 10000);
       </script>
